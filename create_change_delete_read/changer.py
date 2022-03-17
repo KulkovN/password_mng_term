3942d5ed06e9d5f3fddb34d832cd6fb5d0f7ddb2
Nikita Kulkov <culkovn@yandex.ru>
2022-03-17 23:42:43 +0300

    Функция парсера разита на две     что бы была возможность зациклить проверку     ввода пользователя на ключе под замену

--- create_change_delete_read/changer.py
+++ create_change_delete_read/changer.py
@@ -5,6 +5,11 @@ sys.path.append(os.path.join(os.getcwd(), ''))
 from utils.texts import VARABLES
 from all_keys_srvices.show_all_serv import checkin, show_all
 
+
+TEXTS = [f'Вы ввели значение ключа, которого нет в описание вашего серсива. \
+Попробуйте езе раз: ']
+
+
 def changer_js(path:str) -> None:
     """
     Парсит и вносит изменения +
@@ -17,19 +22,27 @@ def changer_js(path:str) -> None:
     service = input('введите сервис, данные которого\
  нужно заменить: ')
     if not checkin(service, data):
-        print('Выбранный отсутствует. Попробуйте еще раз...')
+        print('Такой сервис отсутствует. Попробуйте еще раз')
         changer_js(path)
-    key = input('Введите что вы хотите заменить(service / login / password): ')
-    if key not in VARABLES:
-        key = input(f'Вы ввели {key}! Если уверены, что хотите \
-добавить новый "ключ", повторите ввод. \
-В противном случае - введите корректное значение ключа')
-    value = input('Введите значение для замены: ')
-    for i in data['Loggins & passwords']:
-        if i['service'] == service:
-            i[key] = value
-    with open(path, 'w') as js_f:
-        json.dump(data, js_f, ensure_ascii=False, indent=4)
-        print(f' Данные по {service} в {key} \
-изменены на {value}')
+    key = input(f'Что меняем? (service/login/password): ')
+    write_change(key, data, service, path)
 
+
+def write_change(key:str, data:dict, service:str, path:str) -> None:
+    """
+    Функция парсера разита на две
+    что бы была возможность зациклить проверку
+    ввода пользователя на ключе под замену
+
+    """
+    if key in VARABLES:
+        value = input('Введите значение для замены: ')
+        for i in data['Loggins & passwords']:
+            if i['service'] == service:
+                i[key] = value
+        with open(path, 'w') as js_f:
+            json.dump(data, js_f, ensure_ascii=False, indent=4)
+            print(f' Данные {service} в {key} изменены на {value}')
+    else:
+        key = input(TEXTS[0])
+        write_change(key, data, service, path)

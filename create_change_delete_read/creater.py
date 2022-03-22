82296ed64bde24defdee699de4abfe1b79fb5e92
Nikita Kulkov <culkovn@yandex.ru>
2022-03-22 11:57:06 +0300

    добавлена проверка сущесвтующего списка данных, с целью исключения записи дубликатов

--- create_change_delete_read/creater.py
+++ create_change_delete_read/creater.py
@@ -9,28 +9,39 @@ def path_finder(func:object) -> str:
     если файла нет - то создается / 
     файл для хранения паролей 
     """
-    def wrap(path:str, data:dict) -> None: # обертка
+    def wrap(path:str) -> None: # обертка
         if os.path.exists(path):
-            func(path, data)
+            func(path)
             print(f'\nФайл найден, данные записаны в:\n{path}')
         else:
             with open(path, 'w', encoding='utf-8') as file:
                 json.dump({'Loggins & passwords':[]}, file,
                     ensure_ascii=False, indent=3)
             print(f'\nФайл отсутствует. Создан файл:\n{path}')
-            wrap(path, data)
+            wrap(path)
     return wrap
 
 
 @path_finder
-def write_new_pass(save_path:str, data:dict) -> None:
+def write_new_pass(save_path:str) -> None: #, data:dict) -> None:
     """
     Запись данных в json
     :param save_path (str): Путь до файла хранения данных
-    :param data (dict): данные под запись
+    # :param data (dict): данные под запись
     """
+    uid = {i:input(f'Введите ваш {i}: ') for i in \
+                    dict(service='', login='', password='')} # user input data (uid)
     with open(save_path, 'r+', encoding='utf-8') as file:
         js_data = json.load(file)
-    js_data['Loggins & passwords'].append(data)
-    with open(save_path, 'w', encoding='utf-8') as f:
-        json.dump(js_data, f, ensure_ascii=False, indent=3)
+    if uid['service'] in \
+        [name_serv['service'] for name_serv in js_data['Loggins & passwords']]:
+        print(f"{uid['service']} уже записан в файл. Выберете другое имя сервиса...")
+        write_new_pass(save_path)
+    else:
+        js_data['Loggins & passwords'].append(uid)
+        with open(save_path, 'w', encoding='utf-8') as f:
+            json.dump(js_data, f, ensure_ascii=False, indent=3)
+
+
+# if __name__ == "__main__":
+#     write_new_pass('/Users/kulkovni/Desktop/.allpwd.json')

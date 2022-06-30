9fe8f42e1ad58b6fb54f093bbf03b17816e119ea
Nikita Kulkov <culkovn@yandex.ru>
2022-06-30 12:37:40 +0300

    update reqruetments.txt

--- utils/_crypt.py
+++ utils/_crypt.py
@@ -1,7 +1,7 @@
 # неободим поиск алтренативного решения !! 
 
 import os
-import pyAesCrypt
+from cryptography.fernet import Fernet
 from uuid import uuid4
 from hashlib import sha256
 
@@ -15,39 +15,47 @@ def _hash(pwd) -> str:
     salt = uuid4().hex
     return sha256(salt.encode() + pwd.encode()).hexdigest() + ':' + salt
 
-
-def check_pwd(h_pwd:str, pwd) -> bool:
-    """ Проверка пароля пользователя
-
-    :param h_pwd: захешованый пароль из конфига
-    :param pwd: пароль пользователя при входе
-    :return: булево-значение проверки
-    """
-    password, salt = h_pwd.split(':')
-    return password == sha256(salt.encode() + pwd.encode()).hexdigest()
+# def test(string:str) -> str:
+#     key = Fernet.generate_key()
+#     print(key)
 
 
-def crypt(file, paswd, flag):
-    """ Функция для шифровки и проверки
-    шифрования существующего файла
 
-    :param file: файл для шифрования
-    :param paswd: пароль для шифрования
-    :param flag: флаг шифрования
-    """
-    buffer = 512 * 1024
-    # шифруем 
-    if flag == 'crypt':
-        pyAesCrypt.encryptFile(file, f'{file}.aes'\
-            ,paswd, buffer) 
-        os.remove(file)
-    # дешифруем
-    else:
-        pyAesCrypt.decryptFile(file\
-            ,os.path.splitext(file)[0],
-            paswd, buffer)
-        os.remove(file)
+# if __name__ == '__main__':
+#     test('hello, world!')
+
+# def check_pwd(h_pwd:str, pwd) -> bool:
+#     """ Проверка пароля пользователя
+
+#     :param h_pwd: захешованый пароль из конфига
+#     :param pwd: пароль пользователя при входе
+#     :return: булево-значение проверки
+#     """
+#     password, salt = h_pwd.split(':')
+#     return password == sha256(salt.encode() + pwd.encode()).hexdigest()
 
 
-if __name__ == '__main__':
-    crypt('/Users/kulkovni/Desktop/all_pwd.json', '3571', 'crypt')
+# def crypt(file, paswd, flag):
+#     """ Функция для шифровки и проверки
+#     шифрования существующего файла
+
+#     :param file: файл для шифрования
+#     :param paswd: пароль для шифрования
+#     :param flag: флаг шифрования
+#     """
+#     buffer = 512 * 1024
+#     # шифруем 
+#     if flag == 'crypt':
+#         pyAesCrypt.encryptFile(file, f'{file}.aes'\
+#             ,paswd, buffer) 
+#         os.remove(file)
+#     # дешифруем
+#     else:
+#         pyAesCrypt.decryptFile(file\
+#             ,os.path.splitext(file)[0],
+#             paswd, buffer)
+#         os.remove(file)
+
+
+# if __name__ == '__main__':
+#     crypt('/Users/kulkovni/Desktop/all_pwd.json', '3571', 'crypt')

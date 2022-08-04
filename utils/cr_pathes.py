ce99c740ba9bb8febff6962274457fcfaadd0b7f
Nikita Kulkov <culkovn@yandex.ru>
2022-08-04 17:49:51 +0300

    изменен функционал создания первого файла с паролями + добавлен функционал профилей -> Необходима адапатация остального приложения

--- utils/cr_pathes.py
+++ utils/cr_pathes.py
@@ -1,8 +1,10 @@
+import re
 import json
 import configparser
 from pathlib import Path, PurePosixPath
-from utils._crypt import _hash
-from utils.texts import AFTER_CONF_CREATION
+from _crypt import _hash
+from texts import AFTER_CONF_CREATION
+
 
 
 def configur() -> float:
@@ -13,52 +15,48 @@ def configur() -> float:
     config = configparser.ConfigParser()
     path_for_cnfg = Path(f'{Path.home()}/.py_pass/config')
     path_to_confrFile = f'{path_for_cnfg}/config.ini'
-    # try:
     if not Path(path_for_cnfg).exists():
         path_for_cnfg.mkdir(parents=True) # https://docs.python.org/3/library/pathlib.html#pathlib.Path.mkdir
         paswd = input('Придумайте мастер-пароль для доступа к данным: ')
-        path = js_path_cre(input("Введите путь для записи файла с данными: "))
+        profile = input('придумайте название первого профиля: ')
+        raw_profile_path = Path(f'{Path.home()}/.py_pass/profiles/{profile}/')
+        raw_profile_path.mkdir(parents=True)
+        # path = js_path_cre(input("Введите путь для записи файла с данными: "))
         config.add_section('Config')
-        config.set('Config', 'Path', f'{path}.aes') # для шифрования
-        config.set('Config', 'MasterPaswd', \
-            _hash(paswd))
+        # config.set('Config', 'Path', f'{path}.aes') # для шифрования
+        config.set('Config', 'Path', f'{raw_profile_path}_pwds.json.aes') # для шифрования
+        config.set('Config', 'MasterPaswd', _hash(paswd))
+        # добавление секции профилей
+        config.add_section('Profiles')
+        config.set('Profiles', f'{profile}', profile)
         # запись конфига
         with open(path_to_confrFile, 'w') as file:
             config.write(file)
         print(AFTER_CONF_CREATION)
     config.read(path_to_confrFile)
-    pts, check_pswd = \
-        config.get('Config', 'Path'), config.get('Config', 'MasterPaswd')
-    js_run(pts, check_pswd)
-    return pts, check_pswd
+    pts, check_pswd, profile = \
+        config.get('Config', 'Path'), config.get('Config', 'MasterPaswd'), config.get('Profiles', profile)
+    # js_run(pts, check_pswd, profile)
+    js_run(pts)
+    return pts, check_pswd, profile
 
-
-def js_path_cre(path_:str) -> str:
-    """ Рекурсия проверки пути
-    при первом старте
-
-    :param path_: строка пути, заданная пользователем
-    :return: путь, если он может существовать в ос
-    """
-    if Path(path_).exists():
-        path_ = f'{path_}/.all_pwd.json'
-        return path_
-    else:
-        js_path_cre(input(f'Напишите корректный путь (полный): '))
-        
-
-def js_run(path_js:str, paswd:str) -> None:
+    
+def js_run(path_js:str) -> None:
     """ Проверка json наличие / отсутствие
-    + закриптует после создания
-
+    :param profile: ссылка на вызываемый профиль конкретного пользователя
     :param path_js: строка пути с учетом крипты
     :param paswd: пароль из конфига
     """
     path = path_js.split(PurePosixPath(path_js).suffix)[0]
+    print(path)
     if not Path(path).exists(): # .json
         if not Path(path_js).exists(): # .aes
             with open(path, 'w') as jf:
                 json.dump({'Loggins & passwords':[]}, jf)
-            print(f'Новый файл создан по пути {path}')
+            print(f'профиль создан по пути {path}')
         else:
             return True
+
+
+if __name__ == '__main__':
+    cnf = configur()

deb440f9dda61dbd098c07cccc4238fff6dd023d
Nikita Kulkov <culkovn@yandex.ru>
2022-08-08 12:07:53 +0300

    обновлен функционал создания профиля. На текущем этапе разработка данной функции завершена

--- utils/cr_pathes.py
+++ utils/cr_pathes.py
@@ -17,17 +17,15 @@ def configur(profile:str) -> tuple:
     :return: float с путем и паролем для расшифровки 
     """
     config = configparser.ConfigParser()
-    path_for_cnfg = Path(f'{Path.home()}/.py_pass')
-    path_to_confrFile = f'{path_for_cnfg}/config.ini'
     raw_profile_path = Path(f'{Path.home()}/.py_pass/profiles/{profile}')
-
-    if not Path(path_for_cnfg).exists():
-        path_for_cnfg.mkdir(parents=True) # https://docs.python.org/3/library/pathlib.html#pathlib.Path.mkdir
-        paswd = input('Пароль профиля: ')
+    path_to_confrFile = f'{raw_profile_path}/config.ini'
+    if not Path(raw_profile_path).exists():
+        print('Такой профиль не зарегистрирован, поэтому будет создан как новый.')
         raw_profile_path.mkdir(parents=True)
+        paswd = input('Введите мастер - пароль для профиля: ') 
         config.add_section(f'Config_{profile}')
-        config.set(f'Config_{profile}', f'Profile_{profile}_file', f'{raw_profile_path}/slp.json.aes') # для шифрования
-        config.set(f'Config_{profile}', f'Profile_{profile}_master_pwd', _hash(paswd))
+        config.set(f'Config', f'Profile_{profile}_file', f'{raw_profile_path}/slp.json.aes') # для шифрования
+        config.set(f'Config', f'Profile_{profile}_master_pwd', _hash(paswd))
         # добавление секции профилей
         config.add_section(f'Profile:{profile}')
         config.set(f'Profile:{profile}', f'{profile}', profile)
@@ -42,9 +40,6 @@ def configur(profile:str) -> tuple:
     # js_run(pts, check_pswd, profile)
     js_run(pts)
     return pts, check_pswd, profile
-
-# def new_profile(config_:str):
-#     pass
 
 
 def js_run(path_js:str) -> None:
